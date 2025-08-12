afdbee3ffb5adba253fb258c8a2e1b4cf31b38a1
sumit-1492 <sumitlala99@gmail.com>
2025-08-12 22:10:42 +0530

    put delete method completeed and tested with render

--- testPutDelete.py
+++ testPutDelete.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI(title = " simple API for put and delete")
+app = FastAPI(title = "simple API for put and delete")
 
 userDb = {
     1: {"name": "Sumit",   "age":33},
@@ -9,3 +9,25 @@ userDb = {
     3: {"name": "Trupti" , "age":26}
 }
 
+class userClass(BaseModel):
+    name: str
+    age: int
+
+@app.put("/userdb/database/v1/update/{user_id}")
+def userUpdate(user_id:int,user:userClass):
+    if user_id in userDb:
+        userDb[user_id] = user.model_dump()
+        print(userDb)
+        return {"message" : "User updated succesfully" , "user":userDb[user_id]}
+    else:
+        return {"message" : "User not found"}
+    
+
+@app.delete("/userdb/database/v1/delete/{user_id}")
+def userDelete(user_id:int,user:userClass):
+    if user_id in userDb:
+        del userDb[user_id]
+        print(userDb)
+        return {"message":"user deleted successfully"}
+    else:
+        return {"message" : "User not found"}
